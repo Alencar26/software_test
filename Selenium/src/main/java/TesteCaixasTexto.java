f1855dd594220f5baf3e4695fd53fb1452630af3
Andre Alencar <aaugusto310@gmail.com>
2022-09-09 17:41:01 -0300

    refactor: refatoração dos métodos utilizando a classe DSL como biblioteca.

--- Selenium/src/main/java/TesteCaixasTexto.java
+++ Selenium/src/main/java/TesteCaixasTexto.java
@@ -1,49 +1,40 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteCaixasTexto {
 
-    final String PATH_WEB_PAGE = "file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html";
+    private final String PATH_WEB_PAGE = "file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html";
+    private WebDriver driver;
+    private DSL dsl;
 
-    public WebDriver instanciarNavegador(String path) {
+    @Before
+    public void inicializar() {
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\andre\\www\\drivers\\Selenium\\geckodriver\\geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().setSize(new Dimension(1200, 765));
-        driver.get(path);
-        return driver;
+        driver.get(PATH_WEB_PAGE);
+        dsl = new DSL(driver);
     }
+
+//    @After
+//    public void finalizar() {
+//        driver.quit();
+//    }
 
     @Test
     public void testeTextField() {
-
-        WebDriver driver = instanciarNavegador(PATH_WEB_PAGE);
-
-        //escrevendo no campo de input
-        driver.findElement(By.id("elementosForm:nome")).sendKeys("Escrevendo no text field.");
-
-        Assert.assertEquals("Escrevendo no text field.",
-                    driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
-
-        driver.quit();
+        dsl.escrever("elementosForm:nome", "Escrevendo no text field.");
+        Assert.assertEquals("Escrevendo no text field.", dsl.obterValorCampoText("elementosForm:nome"));
     }
 
     @Test
     public void interacaoComTextArea() {
-        WebDriver driver = instanciarNavegador(PATH_WEB_PAGE);
-
-        //escrevendo no campo
-        driver.findElement(By.id("elementosForm:sugestoes"))
-                .sendKeys("teste");
-
-        //validando valores do campo.
-        String valorTextArea = driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value");
-
-        Assert.assertEquals("teste", valorTextArea);
-
-        driver.quit();
+        dsl.escrever("elementosForm:sugestoes", "teste");
+        Assert.assertEquals("teste", dsl.obterValorCampoText("elementosForm:sugestoes"));
     }
 }
