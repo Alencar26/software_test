487a02caa12729a729799718f3b6d252a52cfe58
Andre Alencar <aaugusto310@gmail.com>
2022-09-12 11:30:31 -0300

    feat: implementação de métodos da biblioteca para validar comboBox.

--- Selenium/src/main/java/DSL.java
+++ Selenium/src/main/java/DSL.java
@@ -1,5 +1,10 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class DSL {
 
@@ -26,6 +31,64 @@ public class DSL {
     }
 
     public void selecionarCombo(String id, String valor) {
-        
+        WebElement element = driver.findElement(By.id(id));
+        Select combo = new Select(element);
+        combo.selectByVisibleText(valor);
+    }
+
+    public String obterValorCombo(String id) {
+        WebElement element = driver.findElement(By.id(id));
+        Select combo = new Select(element);
+        return combo.getFirstSelectedOption().getText();
+    }
+
+    public List<WebElement> retornaTodosOsItensComboBox(String id) {
+        WebElement element = driver.findElement(By.id(id));
+        Select combo = new Select(element);
+        return combo.getOptions();
+    }
+
+    public boolean procurarItemNaLista(List<WebElement> lista, String valor) {
+        boolean encontrou = false;
+        for(WebElement elemento : lista) {
+            if (elemento.getText().equals(valor)) {
+                encontrou = true;
+                break;
+            }
+        }
+        return encontrou;
+    }
+
+    public List<WebElement> retornaItensSelecionadosComboBox(String id) {
+        WebElement element = driver.findElement(By.id(id));
+        Select combo = new Select(element);
+        return combo.getAllSelectedOptions();
+    }
+
+    public boolean validarItensSelecionados(List<WebElement> lista, String[] itens) {
+        int n = 0;
+        boolean allTrue = false;
+        boolean[] validados = new boolean[itens.length];
+
+        for (int i = 0; i < itens.length; i++) {
+            validados[i] = false;
+        }
+
+        for(WebElement item : lista) {
+            if (item.getText().equals(itens[n])) {
+                validados[n] = true;
+            }
+            n++;
+        }
+
+        for(boolean b : validados) {
+            if(!b){
+                allTrue = false;
+                break;
+            }
+            allTrue = true;
+        }
+
+        return allTrue;
     }
 }
