389f5028a01c694009f83b0d1c40e4fd457140ae
Andre Alencar <aaugusto310@gmail.com>
2022-09-18 10:11:36 -0300

    feat: Implementado controle de navegador.

--- Selenium/src/main/java/framework/core/DriverFactory.java
+++ Selenium/src/main/java/framework/core/DriverFactory.java
@@ -2,6 +2,7 @@ package framework.core;
 
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
@@ -12,8 +13,15 @@ public class DriverFactory {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            System.setProperty("webdriver.gecko.driver", "C:\\Users\\andre\\www\\drivers\\Selenium\\geckodriver\\geckodriver.exe");
-            driver = new FirefoxDriver();
+            switch (Propriedades.browser){
+                case FIREFOX:
+                    System.setProperty("webdriver.gecko.driver", "C:\\Users\\andre\\www\\drivers\\Selenium\\geckodriver\\geckodriver.exe");
+                    driver = new FirefoxDriver();
+                    break;
+                case CHROME:
+                    //aqui colocar o caminho do driver do selenium para Chrome.
+                    driver = new ChromeDriver(); break;
+            }
             driver.manage().window().setSize(new Dimension(1200,765));
         }
         return driver;
--- Selenium/src/main/java/framework/core/Propriedades.java
+++ Selenium/src/main/java/framework/core/Propriedades.java
@@ -3,4 +3,9 @@ package framework.core;
 public class Propriedades {
 
     public static boolean FECHAR_BROWSER = false;
+    public static Browsers browser = Browsers.FIREFOX;
+    public enum Browsers {
+        CHROME,
+        FIREFOX
+    }
 }
